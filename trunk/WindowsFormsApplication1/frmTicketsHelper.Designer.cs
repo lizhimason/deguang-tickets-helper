fba4bd1bdc426ef0d205a54aa69239de7f6d7eea
tony.gj@gmail.com <tony.gj@gmail.com@96d9de8a-60b1-8ccd-b4c1-8acec190df73>
2012-01-09 23:35:44 +0000

    原始版本

---
+++ trunk/WindowsFormsApplication1/frmTicketsHelper.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace DeGuangTicketsHelper
+{
+    partial class frmTicketsHelper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTicketsHelper));
+            this.txtVerificationCode = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.picValidImg = new System.Windows.Forms.PictureBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstMsg = new System.Windows.Forms.ListBox();
+            this.linkComPany = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.picValidImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtVerificationCode
+            // 
+            this.txtVerificationCode.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtVerificationCode.Location = new System.Drawing.Point(74, 76);
+            this.txtVerificationCode.MaxLength = 4;
+            this.txtVerificationCode.Name = "txtVerificationCode";
+            this.txtVerificationCode.Size = new System.Drawing.Size(43, 26);
+            this.txtVerificationCode.TabIndex = 30;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLogin.Location = new System.Drawing.Point(74, 108);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 33);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "登录 ";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // picValidImg
+            // 
+            this.picValidImg.Location = new System.Drawing.Point(122, 76);
+            this.picValidImg.Name = "picValidImg";
+            this.picValidImg.Size = new System.Drawing.Size(78, 26);
+            this.picValidImg.TabIndex = 4;
+            this.picValidImg.TabStop = false;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtUserName.Location = new System.Drawing.Point(74, 12);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(126, 26);
+            this.txtUserName.TabIndex = 10;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPassword.Location = new System.Drawing.Point(74, 44);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(126, 26);
+            this.txtPassword.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "登录名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "验证码";
+            // 
+            // lstMsg
+            // 
+            this.lstMsg.FormattingEnabled = true;
+            this.lstMsg.HorizontalScrollbar = true;
+            this.lstMsg.ItemHeight = 12;
+            this.lstMsg.Location = new System.Drawing.Point(15, 147);
+            this.lstMsg.Name = "lstMsg";
+            this.lstMsg.Size = new System.Drawing.Size(185, 124);
+            this.lstMsg.TabIndex = 9;
+            // 
+            // linkComPany
+            // 
+            this.linkComPany.AutoSize = true;
+            this.linkComPany.Location = new System.Drawing.Point(47, 285);
+            this.linkComPany.Name = "linkComPany";
+            this.linkComPany.Size = new System.Drawing.Size(161, 12);
+            this.linkComPany.TabIndex = 32;
+            this.linkComPany.TabStop = true;
+            this.linkComPany.Text = "深圳市德广信息技术有限公司";
+            this.linkComPany.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkComPany_LinkClicked);
+            // 
+            // frmTicketsHelper
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 298);
+            this.Controls.Add(this.linkComPany);
+            this.Controls.Add(this.lstMsg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.picValidImg);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtVerificationCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmTicketsHelper";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "德广火车票助手";
+            this.Load += new System.EventHandler(this.frmTicketsHelper1_Load);
+            this.Validating += new System.ComponentModel.CancelEventHandler(this.frmTicketsHelper1_Validating);
+            ((System.ComponentModel.ISupportInitialize)(this.picValidImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVerificationCode;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox picValidImg;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstMsg;
+        private System.Windows.Forms.LinkLabel linkComPany;
+    }
+}
